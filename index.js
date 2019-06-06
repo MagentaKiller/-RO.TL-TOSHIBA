d018b25fb50f5dc1172117fd45638c244adb1b4a
MagentaKiller <40354513+MagentaKiller@users.noreply.github.com>
2019-06-06 15:13:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Configuration & Settings
-const yourID = "158063324699951104"; //Instructions on how to get this: https://redd.it/40zgse
+const yourID = "293485395138838528"; //Instructions on how to get this: https://redd.it/40zgse
 const setupCMD = "!createrolemessage";
 const initialMessage = `**React to the messages below to receive the associated role. If you would like to remove the role, simply remove your reaction!**`;
 const embedMessage = `
@@ -8,12 +8,12 @@ React to the emoji that matches the role you wish to receive.
 If you would like to remove the role, simply remove your reaction!
 `;
 const embedFooter = "Role Reactions"; // Must set this if "embed" is set to true
-const roles = ["Hacker", "Artist", "Public Relations", "Intern"];
+const roles = ["Baro", "Riven", "Nitain", "Helmet"];
 const reactions = ["💻", "🖌", "😃", "🆕"]; // For custom emojis, provide the name of the emoji
 const embed = false; // Set to "true" if you want all roles to be in a single embed
 const embedColor = "#dd2423"; // Set the embed color if the "embed" variable is set to true
 const embedThumbnail = true; // Set to "true" if you want to set a thumbnail in the embed
-const embedThumbnailLink = "https://i.imgur.com/P8PD7DD.png"; // The link for the embed thumbnail
+const embedThumbnailLink = "https://imgur.com/Zm6L8gh"; // The link for the embed thumbnail
 const botToken = (process.env.token)
 /**
  * You'll have to set this up yourself! Read more below:
