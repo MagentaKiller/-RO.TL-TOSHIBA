06bdf405a2ecd45cb3cdbb6e14ae52c282790a5f
MagentaKiller <40354513+MagentaKiller@users.noreply.github.com>
2019-06-12 22:49:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const embedMessage = `
 Masinaria de mentionat-persoane-inator
 `;
 const embedFooter = "Role Reactions"; // Must set this if "embed" is set to true
-const roles = ["Nitain", "Riven", "Helmet", "Baro", "Warframeuri", "Reactor", "Catalyst", "Exilus", "Aura"];
-const reactions = ["Nitain", "RivenMod", "HelmetBlueprint", "VoidTraderBaro", "excal", "Reactor", "Catalyst", "Exilus", "AuraMod"]; // For custom emojis, provide the name of the emoji
+const roles = ["Nitain", "Riven", "Helmet", "Baro", "Warframeuri", "Reactor", "Catalyst", "Exilus", "Aura", "SeturiPrime"];
+const reactions = ["Nitain", "RivenMod", "HelmetBlueprint", "VoidTraderBaro", "excal", "Reactor", "Catalyst", "Exilus", "AuraMod", "Anarhie"]; // For custom emojis, provide the name of the emoji
 const embed = true; // Set to "true" if you want all roles to be in a single embed
 const embedColor = "#ed0886"; // Set the embed color if the "embed" variable is set to true
 const embedThumbnail = true; // Set to "true" if you want to set a thumbnail in the embed
