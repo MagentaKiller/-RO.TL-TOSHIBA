7719bd888780d8a047773c3febe6ec2c05b2701d
MagentaKiller <40354513+MagentaKiller@users.noreply.github.com>
2019-11-18 20:11:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // Configuration & Settings
 const yourID = "293485395138838528"; //Instructions on how to get this: https://redd.it/40zgse
 const setupCMD = "!on";
-const initialMessage = `Fiecare react are cate un rol separat`;
+const initialMessage = ``;
 const embedMessage = `
-Masinaria de mentionat-persoane-inator
+You will be mentioned when an item will apear!
 `;
 const embedFooter = "Role Reactions"; // Must set this if "embed" is set to true
-const roles = ["Warframeuri", "SeturiPrime", "Nitain", "Riven", "Helmet", "Baro", "Reactor", "Catalyst", "Exilus", "Aura"];
-const reactions = ["excal", "Anarhie", "Nitain", "RivenMod", "HelmetBlueprint", "VoidTraderBaro", "Reactor", "Catalyst", "Exilus", "AuraMod"]; // For custom emojis, provide the name of the emoji
+const roles = ["Nitain", "Riven", "Helmet", "Baro", "Reactor", "Catalyst", "Exilus", "Aura"];
+const reactions = ["Nitain", "RivenMod", "HelmetBlueprint", "VoidTraderBaro", "Reactor", "Catalyst", "Exilus", "AuraMod"]; // For custom emojis, provide the name of the emoji
 const embed = true; // Set to "true" if you want all roles to be in a single embed
 const embedColor = "#ed0886"; // Set the embed color if the "embed" variable is set to true
 const embedThumbnail = true; // Set to "true" if you want to set a thumbnail in the embed
